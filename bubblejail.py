8ae29f91dd408d0469cbdc32ca1fa427f84ab3c8
igo95862 <igo95862@yandex.ru>
2019-11-14 14:03:06 -0800

    Bash now works inside the container

--- bubblejail.py
+++ bubblejail.py
@@ -1,7 +1,9 @@
-from subprocess import run as sub_run
-from typing import List, Tuple
+from subprocess import Popen
+from typing import List, Tuple, IO
+from os import getuid, getgid
 from dataclasses import dataclass, field
 from sys import argv
+from tempfile import TemporaryFile
 
 
 @dataclass
@@ -18,7 +20,7 @@ class ReadOnlyBind(BwrapConfigBase):
     source: str
     dest: str
 
-    def to_args(self):
+    def to_args(self) -> Tuple[str, str, str]:
         return self.arg_word, self.source, self.dest
 
 
@@ -27,7 +29,7 @@ class DirCreate(BwrapConfigBase):
     arg_word = '--dir'
     dest: str
 
-    def to_args(self):
+    def to_args(self) -> Tuple[str, str]:
         return self.arg_word, self.dest
 
 
@@ -37,15 +39,22 @@ class Symlink(BwrapConfigBase):
     source: str
     dest: str
 
-    def to_args(self):
+    def to_args(self) -> Tuple[str, str, str]:
         return self.arg_word, self.source, self.dest
 
 
 @dataclass
+class FileTransfer:
+    content: bytes
+    dest: str
+
+
+@dataclass
 class BwrapArgs:
-    read_only_binds: List[ReadOnlyBind]
-    dir_create: List[DirCreate]
-    symlinks: List[Symlink]
+    read_only_binds: List[ReadOnlyBind] = field(default_factory=list)
+    dir_create: List[DirCreate] = field(default_factory=list)
+    symlinks: List[Symlink] = field(default_factory=list)
+    files: List[FileTransfer] = field(default_factory=list)
     share_network: bool = False
 
 
@@ -55,14 +64,32 @@ DEFAULT_CONFIG = BwrapArgs(
         ReadOnlyBind('/etc/resolv.conf', '/etc/resolv.conf')
     ],
 
-    dir_create=[DirCreate('/tmp'), DirCreate('/var')],
+    dir_create=[DirCreate('/tmp'), DirCreate('/var'), DirCreate('/home/user')],
     symlinks=[Symlink('usr/lib', '/lib'), Symlink('usr/lib64', '/lib64'),
               Symlink('usr/bin', '/bin'), Symlink('usr/sbin', '/sbin')],
+
+    files=[
+        FileTransfer(
+            bytes(f'user:x:{getuid()}:{getuid()}::/home/user:/bin/sh',
+                  encoding='utf-8'),
+            '/etc/passwd'),
+
+        FileTransfer(bytes(f'user:x:{getgid()}:', encoding='utf-8'),
+                     '/etc/group'),
+    ],
 
 )
 
 
-def run_bwrap(args_to_target: list, bwrap_config: BwrapArgs = DEFAULT_CONFIG):
+def copy_data_to_temp_file(data: bytes) -> IO[bytes]:
+    temp_file = TemporaryFile()
+    temp_file.write(data)
+    temp_file.seek(0)
+    return temp_file
+
+
+def run_bwrap(args_to_target: List[str],
+              bwrap_config: BwrapArgs = DEFAULT_CONFIG) -> 'Popen[bytes]':
     bwrap_args: List[str] = ['bwrap']
 
     for ro_entity in bwrap_config.read_only_binds:
@@ -86,8 +113,21 @@ def run_bwrap(args_to_target: list, bwrap_config: BwrapArgs = DEFAULT_CONFIG):
     if bwrap_config.share_network:
         bwrap_args.append('--share-net')
 
+    # Copy files
+    # Prevent our temporary file from being garbage collected
+    temp_files: List[IO[bytes]] = []
+    file_descriptors_to_pass: List[int] = []
+    for f in bwrap_config.files:
+        temp_f = copy_data_to_temp_file(f.content)
+        temp_files.append(temp_f)
+        temp_file_descriptor = temp_f.fileno()
+        file_descriptors_to_pass.append(temp_file_descriptor)
+        bwrap_args.extend(('--file', str(temp_file_descriptor), f.dest))
+
     bwrap_args.extend(args_to_target)
-    return sub_run(bwrap_args)
+    p = Popen(bwrap_args, pass_fds=file_descriptors_to_pass)
+    p.wait()
+    return p
 
 
 if __name__ == "__main__":
