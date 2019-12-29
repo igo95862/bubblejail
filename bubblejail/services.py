dfa59b2efc438dc4285b7a7823b70d10105da6f5
igo95862 <igo95862@yandex.ru>
2019-12-29 16:12:21 +0300

    Added Wayland service

--- bubblejail/services.py
+++ bubblejail/services.py
@@ -1,10 +1,17 @@
-from .bwrap_config import (BwrapArgs, Bind, ReadOnlyBind)
+from .bwrap_config import (BwrapArgs, Bind, ReadOnlyBind,
+                           EnvrimentalVar)
 
 X11 = BwrapArgs(
     binds=[Bind('/tmp/.X11-unix/X0')],
     env_no_unset={'DISPLAY', },
     read_only_binds=[ReadOnlyBind('/etc/fonts/fonts.conf')],
 )
+
+Wayland = BwrapArgs(
+    binds=[Bind('/run/user/1000/wayland-0'),
+           Bind('/run/user/1000/wayland-0.lock')],
+    env_no_unset={'WAYLAND_DISPLAY', 'XDG_RUNTIME_DIR'},
+    enviromental_variables=[EnvrimentalVar('GDK_BACKEND', 'wayland')])
 
 Network = BwrapArgs()
 Network.share_network = True
