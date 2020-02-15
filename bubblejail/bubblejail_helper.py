bfed064abdd4368596661154ef9a99974d2ba87d
igo95862 <igo95862@yandex.ru>
2020-02-14 23:33:14 -0800

    More work on helper
    Implemented simple ping server that pings pongs over unix socket
    Also implemented the child watcher logic.

--- bubblejail/bubblejail_helper.py
+++ bubblejail/bubblejail_helper.py
@@ -14,43 +14,145 @@
 # You should have received a copy of the GNU General Public License
 # along with bubblejail.  If not, see <https://www.gnu.org/licenses/>.
 
-from asyncio import (StreamReader, StreamWriter, create_task,
-                     open_unix_connection)
-from asyncio import run as async_run
-from os import _exit
+from asyncio import (Event, StreamReader, StreamWriter, create_task, sleep,
+                     start_unix_server)
+from json import dumps as json_dumps
+from json import loads as json_loads
+from os import P_NOWAIT, waitpid
 from pathlib import Path
-from typing import Optional
+from typing import Any, Dict, Literal, Optional
+
+RpcMethods = Literal['ping']
+
+
+class JsonRpcBase:
+
+    def __init__(self, request_id: Optional[str]):
+        self.request_id = request_id
+
+    @staticmethod
+    def _encode_helper(dict_form: Dict[str, Any]) -> bytes:
+        string_form = json_dumps(dict_form) + '\n'
+        return string_form.encode()
+
+    @staticmethod
+    def _decode_helper(data: bytes) -> Dict[str, Any]:
+        decoded_dict: Dict[str, Any] = json_loads(data)
+        # Replace 'id' with 'request_id'
+        decoded_dict['request_id'] = decoded_dict['id']
+        decoded_dict.pop('id')
+
+        return decoded_dict
+
+
+class JsonRpcRequest(JsonRpcBase):
+    def __init__(self, method: RpcMethods, request_id: Optional[str] = None):
+        super().__init__(request_id)
+        self.method = method
+
+    def encode(self) -> bytes:
+        dict_form = {
+            "id": self.request_id,
+            "method": self.method,
+        }
+        return self._encode_helper(dict_form)
+
+    @classmethod
+    def decode(cls, data: bytes) -> 'JsonRpcRequest':
+        return JsonRpcRequest(**cls._decode_helper(data))
+
+
+class JsonRpcResponce(JsonRpcBase):
+    def __init__(self,
+                 result: Dict[str, Any],
+                 request_id: Optional[str] = None):
+        super().__init__(request_id)
+        self.result = result
+
+    def encode(self) -> bytes:
+        dict_form = {
+            "id": self.request_id,
+            "result": self.result,
+        }
+        return self._encode_helper(dict_form)
+
+    @classmethod
+    def decode(cls, data: bytes) -> 'JsonRpcResponce':
+        return JsonRpcResponce(**cls._decode_helper(data))
 
 
 class BubblejailHelper:
     def __init__(
-            self, helper_socket_path: Path = Path('/tmp/bubble_sock')):
+        self,
+            helper_socket_path: Path = Path('/tmp/bubble_sock'),
+            no_child_timeout: int = 3,
+            reaper_pool_timer: int = 5,
+    ):
         self.helper_socket_path = helper_socket_path
-        if not helper_socket_path.exists():
-            print('Did not find helper socket')
-            _exit(1)
+        # Event needs to initialized in async function
+        self.termination: Optional[Event] = None
 
-        self.helper_in: Optional[StreamReader] = None
-        self.helper_out: Optional[StreamWriter] = None
+        # Child reaper variables
+        self.no_child_timeout = no_child_timeout
+        self.no_child_countdown = no_child_timeout
+        self.reaper_pool_timer = reaper_pool_timer
 
-    async def helper_input(self) -> None:
-        if self.helper_in is None:
-            raise TypeError()
+    async def child_reaper(self) -> None:
+        if self.termination is None:
+            raise ValueError('Termnination event not initialized')
 
         while True:
-            line = self.helper_in.readline()
+            await sleep(self.reaper_pool_timer)  # wait timer
+            try:
+                while True:
+                    # This loop tries to reap as many children as possible
+                    pid, _ = waitpid(0, P_NOWAIT)
+                    # If we managed to call waitpid reset termination timer
+                    self.no_child_countdown = self.no_child_timeout
+                    if pid == 0:
+                        break
+            except ChildProcessError:
+                # When there are no children start the termnination countdown
+                self.no_child_countdown -= 1
+                print(
+                    'No child present. Countdown: ',
+                    self.no_child_countdown,
+                )
+                if not self.no_child_countdown:
+                    self.termination.set()
+                    return
+
+    async def client_handler(
+            self,
+            reader: StreamReader,
+            writter: StreamWriter) -> None:
+        print('Client connected')
+        while True:
+            line = await reader.readline()
             if not line:
+                print('Reached end of reader. Returnning')
+                writter.close()
+                await writter.wait_closed()
                 return
 
+            request = JsonRpcRequest.decode(line)
+            if request.method == 'ping':
+                responce = JsonRpcResponce(
+                    result={'pong': 'pong'},
+                    request_id=request.request_id,
+                )
+
+                writter.write(responce.encode())
+                await writter.drain()
+
     async def helper_main(self) -> None:
-        (helper_in, helper_out) = await open_unix_connection(
-            path=self.helper_socket_path)
-
-        self.helper_in = helper_in
-        self.helper_out = helper_out
-
-        input_task = create_task(self.helper_input())
-        await input_task
-
-
-async_run(BubblejailHelper().helper_main())
+        server = await start_unix_server(
+            self.client_handler,
+            path=self.helper_socket_path,
+        )
+        print('Started unix server')
+        self.termination = Event()
+        create_task(self.child_reaper())
+        await self.termination.wait()
+        server.close()
+        print('Terminated')
