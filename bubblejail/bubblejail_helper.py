9fbeaad910360e6c26ff9286f4767ea7e039d5bd
igo95862 <igo95862@yandex.ru>
2020-02-23 00:54:36 -0800

    Helper mostly finished.

--- bubblejail/bubblejail_helper.py
+++ bubblejail/bubblejail_helper.py
@@ -14,95 +14,159 @@
 # You should have received a copy of the GNU General Public License
 # along with bubblejail.  If not, see <https://www.gnu.org/licenses/>.
 
-from asyncio import (Event, StreamReader, StreamWriter, create_task, sleep,
-                     start_unix_server)
+from asyncio import (AbstractServer, CancelledError, Event, StreamReader,
+                     StreamWriter, Task, create_subprocess_exec, create_task,
+                     gather)
+from asyncio import run as async_run
+from asyncio import sleep, start_unix_server
+from asyncio.subprocess import DEVNULL, PIPE, STDOUT
 from json import dumps as json_dumps
 from json import loads as json_loads
 from os import P_NOWAIT, waitpid
 from pathlib import Path
-from typing import Any, Dict, Literal, Optional
+from sys import argv
+from typing import (Any, Awaitable, Dict, Generator, List, Literal, Optional,
+                    Union)
 
-RpcMethods = Literal['ping']
+from xdg.BaseDirectory import get_runtime_dir
+
+# region Rpc
+RpcMethods = Literal['ping', 'run']
+RpcData = Union[Dict[str, Union[str, List[str]]], List[str]]
+RpcType = Dict[str, Optional[Union[str, RpcData, RpcMethods]]]
 
 
-class JsonRpcBase:
-
-    def __init__(self, request_id: Optional[str]):
-        self.request_id = request_id
-
+class JsonRpcRequest:
     @staticmethod
-    def _encode_helper(dict_form: Dict[str, Any]) -> bytes:
-        string_form = json_dumps(dict_form) + '\n'
+    def _dict_to_json_byte_line(rpc_dict: Dict[Any, Any]) -> bytes:
+        string_form = json_dumps(rpc_dict) + '\n'
         return string_form.encode()
 
     @staticmethod
-    def _decode_helper(data: bytes) -> Dict[str, Any]:
-        decoded_dict: Dict[str, Any] = json_loads(data)
+    def _json_byte_line_to_dict(data: bytes) -> Dict[Any, Any]:
+        decoded_dict: Dict[Any, Any] = json_loads(data)
         # Replace 'id' with 'request_id'
         decoded_dict['request_id'] = decoded_dict['id']
         decoded_dict.pop('id')
 
         return decoded_dict
 
-
-class JsonRpcRequest(JsonRpcBase):
-    def __init__(self, method: RpcMethods, request_id: Optional[str] = None):
-        super().__init__(request_id)
+    def __init__(self,
+                 method: RpcMethods,
+                 request_id: Optional[str] = None,
+                 params: Optional[RpcData] = None,
+                 ):
+        self.request_id = request_id
         self.method = method
+        self.params = params
 
-    def encode(self) -> bytes:
+    def to_json_byte_line(self) -> bytes:
         dict_form = {
             "id": self.request_id,
             "method": self.method,
+            "params": self.params,
         }
-        return self._encode_helper(dict_form)
+        return self._dict_to_json_byte_line(dict_form)
 
-    @classmethod
-    def decode(cls, data: bytes) -> 'JsonRpcRequest':
-        return JsonRpcRequest(**cls._decode_helper(data))
-
-
-class JsonRpcResponce(JsonRpcBase):
-    def __init__(self,
-                 result: Dict[str, Any],
-                 request_id: Optional[str] = None):
-        super().__init__(request_id)
-        self.result = result
-
-    def encode(self) -> bytes:
-        dict_form = {
+    def _get_reponse_bytes(self, rpc_data: RpcData) -> bytes:
+        response_dict = {
             "id": self.request_id,
-            "result": self.result,
+            "result": rpc_data,
         }
-        return self._encode_helper(dict_form)
-
-    @classmethod
-    def decode(cls, data: bytes) -> 'JsonRpcResponce':
-        return JsonRpcResponce(**cls._decode_helper(data))
+        return self._dict_to_json_byte_line(response_dict)
 
 
-class BubblejailHelper:
+class RequestPing(JsonRpcRequest):
+    def __init__(self, request_id: Optional[str] = None) -> None:
+        super().__init__(
+            method='ping',
+            request_id=request_id,
+        )
+
+    def response_ping(self) -> bytes:
+        return self._get_reponse_bytes(['pong'])
+
+
+class RequestRun(JsonRpcRequest):
     def __init__(
         self,
-            helper_socket_path: Path = Path('/tmp/bubble_sock'),
-            no_child_timeout: int = 3,
+        args_to_run: List[str],
+        request_id: Optional[str] = None
+    ) -> None:
+        super().__init__(
+            method='run',
+            request_id=request_id,
+            params=args_to_run,
+        )
+        self.args_to_run = args_to_run
+
+    def response_run(self, text: str) -> bytes:
+        return self._get_reponse_bytes({'return': text})
+
+
+RpcRequests = Union[RequestPing, RequestRun]
+
+
+def request_selector(data: bytes) -> RpcRequests:
+    decoded_dict = JsonRpcRequest._json_byte_line_to_dict(data)
+
+    method = decoded_dict['method']
+    request_id = decoded_dict['request_id']
+    params = decoded_dict['params']
+
+    if method == 'ping':
+        return RequestPing(request_id=request_id)
+    elif method == 'run':
+        return RequestRun(
+            request_id=request_id,
+            args_to_run=params,
+        )
+    else:
+        raise TypeError('Unknown rpc method.')
+# endregion Rpc
+
+
+class BubblejailHelper(Awaitable[bool]):
+    def __init__(
+        self,
+            helper_socket_path: Path = Path('/run/bubblehelp/helper.socket'),
+            no_child_timeout: Optional[int] = 3,
             reaper_pool_timer: int = 5,
+            use_fixups: bool = True,
+            exec_argv: bool = True,
     ):
         self.helper_socket_path = helper_socket_path
-        # Event needs to initialized in async function
-        self.termination: Optional[Event] = None
+
+        # Server
+        self.server: Optional[AbstractServer] = None
+
+        # Event terminated
+        self.terminated = Event()
 
         # Child reaper variables
         self.no_child_timeout = no_child_timeout
         self.no_child_countdown = no_child_timeout
         self.reaper_pool_timer = reaper_pool_timer
+        self.child_reaper_task: Optional[Task[None]] = None
+
+        self.exec_argv = exec_argv
+
+        # Fix-ups
+        if not use_fixups:
+            return
+
+        # Make sure that XDG_RUNTIME_DIR is 700
+        # otherwise KDE applicaitons do not work
+        Path(get_runtime_dir()).chmod(0o700)
 
     async def child_reaper(self) -> None:
-        if self.termination is None:
-            raise ValueError('Termnination event not initialized')
 
         while True:
-            await sleep(self.reaper_pool_timer)  # wait timer
+            try:
+                await sleep(self.reaper_pool_timer)  # wait timer
+            except CancelledError:
+                return
+
             try:
                 while True:
                     # This loop tries to reap as many children as possible
@@ -112,48 +176,119 @@ class BubblejailHelper:
                     if pid == 0:
                         break
             except ChildProcessError:
-                # When there are no children start the termnination countdown
-                self.no_child_countdown -= 1
-                print(
-                    'No child present. Countdown: ',
-                    self.no_child_countdown,
+                # If no_child_countdown is set to None means we don't want
+                # the helper to shutdown because there are no children
+                if self.no_child_countdown is not None:
+                    # When there are no children start
+                    # the termnination countdown
+                    self.no_child_countdown -= 1
+                    print(
+                        'No child present. Countdown: ',
+                        self.no_child_countdown,
+                        flush=True,
+                    )
+                    if not self.no_child_countdown:
+                        create_task(self.stop_async())
+                        return
+
+    async def run_command(
+            self,
+            args_to_run: List[str],
+            wait_completion: bool = False,) -> Optional[str]:
+
+        if wait_completion:
+            p = await create_subprocess_exec(
+                *args_to_run,
+                stdout=PIPE,
+                stderr=STDOUT,
+                stdin=PIPE,
+            )
+            (stdout_data, _) = await p.communicate()
+            return str(stdout_data)
+        else:
+            create_task(
+                create_subprocess_exec(
+                    *args_to_run,
+                    stdout=DEVNULL,
+                    stderr=DEVNULL,
+                    stdin=DEVNULL,
                 )
-                if not self.no_child_countdown:
-                    self.termination.set()
-                    return
+            )
+            return None
 
     async def client_handler(
             self,
             reader: StreamReader,
             writter: StreamWriter) -> None:
-        print('Client connected')
+        print('Client connected', flush=True)
         while True:
             line = await reader.readline()
             if not line:
-                print('Reached end of reader. Returnning')
+                print('Reached end of reader. Returnning', flush=True)
                 writter.close()
                 await writter.wait_closed()
                 return
 
-            request = JsonRpcRequest.decode(line)
-            if request.method == 'ping':
-                responce = JsonRpcResponce(
-                    result={'pong': 'pong'},
-                    request_id=request.request_id,
+            request = request_selector(line)
+
+            if isinstance(request, RequestPing):
+                response = request.response_ping()
+            elif isinstance(request, RequestRun):
+                run_stdout = await self.run_command(
+                    args_to_run=request.args_to_run,
+                    wait_completion=(True if request.request_id is not None
+                                     else False),
                 )
+                if run_stdout is None:
+                    continue
+                else:
+                    response = request.response_run(
+                        text=run_stdout,
+                    )
 
-                writter.write(responce.encode())
-                await writter.drain()
+            writter.write(response)
+            await writter.drain()
 
-    async def helper_main(self) -> None:
-        server = await start_unix_server(
+    async def start_async(self) -> None:
+        self.server = await start_unix_server(
             self.client_handler,
             path=self.helper_socket_path,
         )
-        print('Started unix server')
-        self.termination = Event()
-        create_task(self.child_reaper())
-        await self.termination.wait()
-        server.close()
-        await server.wait_closed()
-        print('Terminated')
+        print('Started unix server', flush=True)
+        self.child_reaper_task = create_task(self.child_reaper())
+        if self.exec_argv:
+            print(argv, flush=True)
+            if len(argv) > 1:
+                await self.run_command(argv[1:])
+
+    async def stop_async(self) -> None:
+
+        if (self.child_reaper_task is not None
+            and
+                not self.child_reaper_task.done()):
+            self.child_reaper_task.cancel()
+            await self.child_reaper_task
+
+        if self.server is not None:
+            self.server.close()
+            await self.server.wait_closed()
+
+        self.terminated.set()
+        print('Terminated', flush=True)
+
+    def __await__(self) -> Generator[Any, None, bool]:
+        # Pylint does not recognize that we get a coroutine object
+        # not the boolean
+        # pylint: disable=E1101
+        coroutine = self.terminated.wait()
+        return coroutine.__await__()
+
+
+if __name__ == '__main__':
+    print('Main helper script starts', flush=True)
+
+    async def run_helper() -> None:
+        helper = BubblejailHelper()
+        await gather(helper.start_async(), helper)
+
+    async_run(run_helper())
