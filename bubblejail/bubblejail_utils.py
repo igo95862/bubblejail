98bde882adbf88486dc17290b0c202ff5c2ce1f7
igo95862 <igo95862@yandex.ru>
2020-03-08 11:29:23 -0700

    Services can now be configured at the same time as declared
    If you defined service under services = [] list and when tried to
    configure it with [service.name] it used to generate args twice.
    Now args are generated once based on cofiguration.

--- bubblejail/bubblejail_utils.py
+++ bubblejail/bubblejail_utils.py
@@ -17,7 +17,7 @@
 
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any, Dict, Generator, List, Optional, Union
+from typing import Any, Dict, Generator, List, Optional, Union, Set
 
 from .services import SERVICES, BubblejailService
 
@@ -32,11 +32,15 @@ class BubblejailInstanceConfig:
     service: TypeServicesConfig = field(default_factory=dict)
 
     def iter_services(self) -> Generator[BubblejailService, None, None]:
-        for service_name in self.services:
-            yield SERVICES[service_name]()
+        initialized_services_names: Set[str] = set()
 
         for service_name, service_dict in self.service.items():
+            initialized_services_names.add(service_name)
             yield SERVICES[service_name](**service_dict)
+
+        for service_name in self.services:
+            if service_name not in initialized_services_names:
+                yield SERVICES[service_name]()
 
 
 @dataclass
