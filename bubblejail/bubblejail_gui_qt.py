d3fe091ae4e891d8add53d547038367ba0599007
igo95862 <igo95862@yandex.ru>
2020-07-12 22:31:41 -0700

    Progress on GUI
    Added basic checkboxes and line edits

--- bubblejail/bubblejail_gui_qt.py
+++ bubblejail/bubblejail_gui_qt.py
@@ -15,11 +15,17 @@
 # along with bubblejail.  If not, see <https://www.gnu.org/licenses/>.
 
 from sys import argv
+from typing import Optional, Type
 
 from PyQt5.QtCore import QModelIndex
-from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QListWidget,
-                             QListWidgetItem, QMainWindow, QPushButton,
-                             QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QGroupBox, QHBoxLayout,
+                             QLabel, QLineEdit, QListWidget, QListWidgetItem,
+                             QMainWindow, QPushButton, QVBoxLayout, QWidget)
+
+long_text = ('''aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+ccccccccccccccccccccccccccccccccccccc
+dddddddddddddddddddddddddddddddddddddddddddddd''')
 
 
 class SelectInstanceWidget:
@@ -49,14 +55,93 @@ class InstanceEditWidget:
         self.parent = parent
         self.widget = QWidget()
 
-        bottom_pannel = QHBoxLayout()
-        test_button = QPushButton('Top')
-        test_button.clicked.connect(self.parent.switch_to_selector)
+        self.main_layout = QVBoxLayout()
 
-        bottom_pannel.addWidget(test_button)
-        bottom_pannel.addWidget(QPushButton('Bottom'))
+        header = QHBoxLayout()
+        # Back button
+        back_button = QPushButton('Back')
+        back_button.clicked.connect(self.parent.switch_to_selector)
+        header.addWidget(back_button)
+        # Label
+        header_label = QLabel('Editing ...')
+        header.addWidget(header_label)
+        # Save button
+        save_button = QPushButton('Save')
+        header.addWidget(save_button)
 
-        self.widget.setLayout(bottom_pannel)
+        self.main_layout.addLayout(header)
+
+        # Groups
+        self.main_layout.addWidget(QLabel('General Options'))
+
+        general_group_layout = self.add_group_return_layout(
+            title='Test Group',
+            is_checkable=True
+        )
+
+        self.add_checkbox(
+            'Test',
+            general_group_layout,
+            tooltip_text=long_text,
+        )
+        self.add_line_edit(
+            'Test',
+            general_group_layout,
+            tooltip_text=long_text,
+        )
+
+        self.widget.setLayout(self.main_layout)
+
+    def add_group_return_layout(
+            self, title: str, is_checkable: bool) -> QVBoxLayout:
+        new_group = QGroupBox(title)
+        new_group.setCheckable(is_checkable)
+        group_layout = QVBoxLayout()
+        new_group.setLayout(group_layout)
+        self.main_layout.addWidget(new_group)
+        return group_layout
+
+    def add_widget_return_widget(
+        self,
+        widget_class: Type[QWidget],
+        text: str,
+        parent_layout: QVBoxLayout,
+        tooltip_text: Optional[str] = None,
+    ) -> QWidget:
+        new_widget = widget_class(text)
+
+        if tooltip_text is not None:
+            new_widget.setToolTip(tooltip_text)
+
+        parent_layout.addWidget(new_widget)
+
+        return new_widget
+
+    def add_checkbox(
+        self,
+        text: str,
+        parent_layout: QVBoxLayout,
+        tooltip_text: Optional[str] = None,
+    ) -> QCheckBox:
+        return self.add_widget_return_widget(
+            widget_class=QCheckBox,
+            text=text,
+            parent_layout=parent_layout,
+            tooltip_text=tooltip_text,
+        )
+
+    def add_line_edit(
+        self,
+        text: str,
+        parent_layout: QVBoxLayout,
+        tooltip_text: Optional[str] = None,
+    ) -> QLineEdit:
+        return self.add_widget_return_widget(
+            widget_class=QLineEdit,
+            text=text,
+            parent_layout=parent_layout,
+            tooltip_text=tooltip_text,
+        )
 
 
 class BubblejailConfigApp:
