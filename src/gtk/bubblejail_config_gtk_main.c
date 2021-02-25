71cc01f19b2c8eb6f4f824dbff54fe5b85e4f184
igo95862 <igo95862@yandex.ru>
2021-02-25 14:54:50 +0300

    Added licensing to gtk gui source file

--- src/gtk/bubblejail_config_gtk_main.c
+++ src/gtk/bubblejail_config_gtk_main.c
@@ -1,3 +1,20 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+  Copyright 2019-2021 igo95862, ls0h
+
+  This file is part of bubblejail.
+  bubblejail is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  bubblejail is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  You should have received a copy of the GNU General Public License
+  GNU General Public License for more details.
+  along with bubblejail.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <gtk/gtk.h>
 
 const gint gnome_recommended_horizontal_spacing = 12;
