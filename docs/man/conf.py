51efdce593536d29d2f522a1e2d427e731251bfd
igo95862 <igo95862@yandex.ru>
2023-02-04 15:50:31 +0600

    Remove old man files

--- docs/man/conf.py
+++
@@ -1,9 +0,0 @@
-project = 'bubblejail'
-author = 'igo95862'
-source_suffix = '.rst'
-
-man_pages = [
-    ('bubblejail', 'bubblejail', 'desktop sandboxing utility', 'igo95862', 1),
-]
-
-master_doc = 'bubblejail'
