d1933cb7970a8eca4e5a534721cdfbe72f7d565b
igo95862 <igo95862@yandex.ru>
2023-02-04 20:51:58 +0600

    Added GUI test runner
    Runs GUI in a test environment that does not affect the system.

--- tools/run_test_bubblejail.py
+++ tools/run_test_bubblejail.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
 from os import environ
 from pathlib import Path
 from shlex import split as shelx_split
@@ -7,9 +8,10 @@ from typing import Generator
 
 from bubblejail.bubblejail_cli import bubblejail_main
 from bubblejail.bubblejail_directories import BubblejailDirectories
+from bubblejail.bubblejail_gui_qt import run_gui
 
 
-def shell_main() -> None:
+def setup_test_env() -> None:
     build_dir = Path(environ['MESON_BUILD_ROOT'])
     custom_data_dir = build_dir / 'bubblejail_test_datadir'
     custom_data_dir.mkdir(exist_ok=True)
@@ -23,6 +25,8 @@ def shell_main() -> None:
         custom_datadirs,
     )
 
+
+def shell_main() -> None:
     while True:
         try:
             input_line = input('bubblejail>> ')
@@ -40,5 +44,26 @@ def shell_main() -> None:
             ...
 
 
+def gui_main() -> None:
+    run_gui()
+
+
+TEST_RUNNERS = {
+    'shell': shell_main,
+    'gui': gui_main,
+}
+
+
+def main() -> None:
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(
+        'runner',
+        choices=TEST_RUNNERS.keys(),
+    )
+    args = arg_parser.parse_args()
+    setup_test_env()
+    TEST_RUNNERS[args.runner]()
+
+
 if __name__ == '__main__':
-    shell_main()
+    main()
